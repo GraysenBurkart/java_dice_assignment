d1ad57a5177053ef7972c89243ac651ebd62afda
GraysenBurkart <graysen1025380@my.yosemite.edu>
2025-09-07 21:05:59 -0700

    Created a loop to roll multiple times.

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //creates a new ArrayList
-        ArrayList<String> dice = new ArrayList<String>();
-//            allows for user to input information
+
+        //allows for user to input information
         Scanner userInput = new Scanner(System.in);
+        boolean shouldContinue = true;
 
-//        takes input and closes the input loop
-        System.out.println("How many side do you want your die to have?");
-        int dieSide = userInput.nextInt();
 
-//        takes input and closes the input loop
-        System.out.println("How many dice would you like to roll?");
-        int timesRolled = userInput.nextInt();
+        while (shouldContinue) {
+            System.out.println("Do you want to roll some dice?");
+            String diceRoll = userInput.nextLine();
 
-//        creates a loop
-        for (int i = 0; i < timesRolled; i++) {
-//            creates random number from input
-            int randomNum =  (int) ((Math.random() * dieSide) + 1);
-//            adds number rolled to ArrayList
-            dice.add(Integer.toString(randomNum));
+            if (diceRoll.equalsIgnoreCase("yes")) {
+
+                //creates a new ArrayList
+                var dice = new ArrayList<String>();
+                //takes input and closes the input loop
+                System.out.println("How many dice would you like to roll?");
+                int timesRolled = userInput.nextInt();
+
+                //takes input and closes the input loop
+                System.out.println("How many side do you want your die to have?");
+                int dieSide = userInput.nextInt();
+
+                //creates a loop
+                for (int i = 0; i < timesRolled; i++) {
+                    //creates random number from input
+                    int randomNum = (int) ((Math.random() * dieSide) + 1);
+                    //adds number rolled to ArrayList
+                    dice.add(Integer.toString(randomNum));
+                }
+                //outputs ArrayList
+                System.out.println(dice);
+            } else if (diceRoll.equalsIgnoreCase("no")) {
+                shouldContinue = false;
+                System.out.print("OK... Bye...");
+            }
         }
-//        outputs ArrayList
-        System.out.println(dice);
     }
 }
