0eafcb3af49103a37b383b747953179e6c1455b4
GraysenBurkart <graysen1025380@my.yosemite.edu>
2025-09-18 18:19:10 -0700

    Allows user to request the lowest or highest value rolled.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         //allows for user to input information
         Scanner userInput = new Scanner(System.in);
         boolean shouldContinue = true;
+        //creates a new ArrayList
+        var dice = new ArrayList<Integer>();
+        int timesRolled = 0;
+        int dieSide = 0;
 
 
         while (shouldContinue) {
@@ -18,29 +22,47 @@ public class Main {
 
             if (diceRoll.equalsIgnoreCase("yes")) {
 
-                //creates a new ArrayList
-                var dice = new ArrayList<String>();
                 //takes input and closes the input loop
                 System.out.println("How many dice would you like to roll?");
-                int timesRolled = userInput.nextInt();
+                timesRolled = userInput.nextInt();
 
                 //takes input and closes the input loop
                 System.out.println("How many side do you want your die to have?");
-                int dieSide = userInput.nextInt();
+                dieSide = userInput.nextInt();
 
                 //creates a loop
                 for (int i = 0; i < timesRolled; i++) {
                     //creates random number from input
                     int randomNum = (int) ((Math.random() * dieSide) + 1);
                     //adds number rolled to ArrayList
-                    dice.add(Integer.toString(randomNum));
+                    dice.add(randomNum);
                 }
                 //outputs ArrayList
                 System.out.println(dice);
-            } else if (diceRoll.equalsIgnoreCase("no")) {
-                shouldContinue = false;
-                System.out.print("OK... Bye...");
+                System.out.println("Would you like to know your lowest or highest roll?");
+                String answer = userInput.nextLine();
+                if (answer.equalsIgnoreCase("lowest")) {
+                    int lowestDie = dice.get(0);
+                    for (int die : dice) {
+                        if (lowestDie > die) {
+                            lowestDie = die;
+                        }
+                    }
+                    System.out.println(lowestDie);
+                } else if (answer.equalsIgnoreCase("highest")) {
+                    int highestDie = dice.get(timesRolled);
+                    for (int die : dice) {
+                        if (highestDie < die) {
+                            highestDie = die;
+                        }
+                    }
+                    System.out.println(highestDie);
+                }else if (diceRoll.equalsIgnoreCase("no")) {
+                    shouldContinue = false;
+                    System.out.print("OK... Bye...");
+                }
+                }
             }
         }
     }
-}
+
